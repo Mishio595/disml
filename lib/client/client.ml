527cb01c914bcb13a257bc33107c4573a72cc6dd
Mishio595 <tenseiken595@gmail.com>
2018-11-18 19:07:17 -0700

    Add complete event support for client.on

--- lib/client/client.ml
+++ lib/client/client.ml
@@ -60,9 +60,45 @@ let start ?count client =
     client
 
 let on ev client fn =
-    match ev with
-    | "MESSAGE_CREATE" -> client.handler <- { client.handler with message_create = Some(fn) }
-    | _ -> ()
+    client.handler <- (match ev with
+    | "READY" -> { client.handler with ready = Some(fn) }
+    | "RESUMED" -> { client.handler with resumed = Some(fn) }
+    | "CHANNEL_CREATE" -> { client.handler with channel_create = Some(fn) }
+    | "CHANNEL_DELETE" -> { client.handler with channel_delete = Some(fn) }
+    | "CHANNEL_UPDATE" -> { client.handler with channel_update = Some(fn) }
+    | "CHANNEL_PINS_UPDATE" -> { client.handler with channel_pins_update = Some(fn) }
+    | "GUILD_CREATE" -> { client.handler with guild_create = Some(fn) }
+    | "GUILD_DELETE" -> { client.handler with guild_delete = Some(fn) }
+    | "GUILD_UPDATE" -> { client.handler with guild_update = Some(fn) }
+    | "GUILD_BAN_ADD" -> { client.handler with guild_ban_add = Some(fn) }
+    | "GUILD_BAN_REMOVE" -> { client.handler with guild_ban_remove = Some(fn) }
+    | "GUILD_EMOJIS_UPDATE" -> { client.handler with guild_emojis_update = Some(fn) }
+    | "GUILD_INTEGRATIONS_UPDATE" -> { client.handler with guild_integrations_update = Some(fn) }
+    | "GUILD_MEMBER_ADD" -> { client.handler with guild_member_add = Some(fn) }
+    | "GUILD_MEMBER_REMOVE" -> { client.handler with guild_member_remove = Some(fn) }
+    | "GUILD_MEMBER_UPDATE" -> { client.handler with guild_member_update = Some(fn) }
+    | "GUILD_MEMBERS_CHUNK" -> { client.handler with guild_members_chunk = Some(fn) }
+    | "GUILD_ROLE_CREATE" -> { client.handler with guild_role_create = Some(fn) }
+    | "GUILD_ROLE_DELETE" -> { client.handler with guild_role_delete = Some(fn) }
+    | "GUILD_ROLE_UPDATE" -> { client.handler with guild_role_update = Some(fn) }
+    | "MESSAGE_CREATE" -> { client.handler with message_create = Some(fn) }
+    | "MESSAGE_DELETE" -> { client.handler with message_delete = Some(fn) }
+    | "MESSAGE_UPDATE" -> { client.handler with message_update = Some(fn) }
+    | "MESSAGE_DELETE_BULK" -> { client.handler with message_delete_bulk = Some(fn) }
+    | "MESSAGE_REACTION_ADD" -> { client.handler with message_reaction_add = Some(fn) }
+    | "MESSAGE_REACTION_REMOVE" -> { client.handler with message_reaction_remove = Some(fn) }
+    | "MESSAGE_REACTION_REMOVE_ALL" -> { client.handler with message_reaction_remove_all = Some(fn) }
+    | "PRESENCE_UPDATE" -> { client.handler with presence_update = Some(fn) }
+    | "TYPING_START" -> { client.handler with typing_start = Some(fn) }
+    | "USER_UPDATE" -> { client.handler with user_update = Some(fn) }
+    | "VOICE_STATE_UPDATE" -> { client.handler with voice_state_update = Some(fn) }
+    | "VOICE_SERVER_UPDATE" -> { client.handler with voice_server_update = Some(fn) }
+    | "WEBHOOKS_UPDATE" -> { client.handler with webhooks_update = Some(fn) }
+    | _ -> client.handler);
+    match Ivar.peek client.sharder with
+    | Some s -> Sharder.update_handler s client.handler;
+    | None -> ()
+
 
 let set_status client status =
     Ivar.read client.sharder

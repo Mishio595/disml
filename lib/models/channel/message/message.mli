8662e92987c437f59d09896a247ec2b5d82c4528
Adelyn Breedlove <tenseiken595@gmail.com>
2019-01-28 10:31:51 -0700

    Publish docs updates

--- lib/models/channel/message/message.mli
+++ lib/models/channel/message/message.mli
@@ -2,13 +2,28 @@ open Async
 
 include module type of Message_t
 
+(** Add the given emoji as a reaction. *)
 val add_reaction : t -> Emoji.t -> unit Deferred.Or_error.t
+
+(** Remove the reaction. Must also specify the user. *)
 val remove_reaction : t -> Emoji.t -> User_t.t -> unit Deferred.Or_error.t
+
+(** Remove all reactions from the message. *)
 val clear_reactions : t -> unit Deferred.Or_error.t
+
+(** Delete the message. *)
 val delete : t -> unit Deferred.Or_error.t
+
+(** Pin the message. *)
 val pin : t -> unit Deferred.Or_error.t
+
+(** Unping the message. *)
 val unpin : t -> unit Deferred.Or_error.t
+
+(** Sugar for [Channel_id.say msg.channel_id content]. *)
 val reply : t -> string -> t Deferred.Or_error.t
+
+(** Sugar for [Channel_id.send_message ?embed ?content ?file ?tts msg.channel_id]. *)
 val reply_with :
     ?embed:Embed.t ->
     ?content:string ->
@@ -16,5 +31,9 @@ val reply_with :
     ?tts:bool ->
     t ->
     Message_t.t Deferred.Or_error.t
+    
+(** Set the content of the message. *)
 val set_content : t -> string -> t Deferred.Or_error.t
+
+(** Set the embed of the message. *)
 val set_embed : t -> Embed.t -> t Deferred.Or_error.t
--- lib/models/channel/message/message_t.mli
+++ lib/models/channel/message/message_t.mli
@@ -1,22 +1,22 @@
 (** Represents a message object. *)
 type t = {
-    id: Message_id.t;
-    author: User_t.t;
-    channel_id: Channel_id_t.t;
-    member: Member_t.partial_member option;
-    guild_id: Guild_id_t.t option;
-    content: string;
-    timestamp: string;
-    edited_timestamp: string option;
-    tts: bool;
-    mention_everyone: bool;
-    mentions: User_t.t list;
-    mention_roles: Role_id.t list;
-    attachments: Attachment.t list;
-    embeds: Embed.t list;
-    reactions: Snowflake.t list;
-    nonce: Snowflake.t option;
-    pinned: bool;
-    webhook_id: Snowflake.t option;
-    kind: int;
+    id: Message_id.t; (** Snowflake ID of the message. *)
+    author: User_t.t; (** User that authored the message. *)
+    channel_id: Channel_id_t.t; (** Channel ID the message exists in. *)
+    member: Member_t.partial_member option; (** A partial member if the message was sent in a guild. *)
+    guild_id: Guild_id_t.t option; (** Guild ID if the message was sent in a guild. *)
+    content: string; (** Content of the message. *)
+    timestamp: string; (** ISO8601 timestamp of when the message was created. *)
+    edited_timestamp: string option; (** Like timestamp, but for last edit, if any. *)
+    tts: bool; (** Whether the message used text-to-speech. *)
+    mention_everyone: bool; (** Whether the message mentioned [@everyone] or [@here] *)
+    mentions: User_t.t list; (** A List of users that were mentioned in the message. *)
+    mention_roles: Role_id.t list; (** A list of roles that were mentioned in the message. *)
+    attachments: Attachment.t list; (** A list of attachments. *)
+    embeds: Embed.t list; (** A List of embeds on the message. *)
+    reactions: Snowflake.t list; (** A list of reactions. *)
+    nonce: Snowflake.t option; (** Used in verification, safe to ignore. *)
+    pinned: bool; (** Whether the message is pinned. *)
+    webhook_id: Snowflake.t option; (** The webhook ID, if the message was sent by a webhook. *)
+    kind: int; (** See {{:https://discordapp.com/developers/docs/resources/channel#message-object-message-types}the discord docs} for message type enumeration. *)
 } [@@deriving sexp, yojson]
